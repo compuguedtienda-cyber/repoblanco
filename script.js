61e626ff941563566995fcadbd98358fb8e435d5
syncpc <josemgued@users.noreply.github.com>
2025-10-16 16:10:26 -0500

    nvo
    ahora

--- script.js
+++ script.js
@@ -47,3 +47,22 @@ const swiper = new Swiper('.slider-wrapper', {
     }
   }
 });
+
+// SLIDER
+let sliderInner =
+document.querySelector(".slider--inner");
+
+let images =
+sliderInner.querySelectorAll("img");
+
+let index = 1;
+
+setInterval(function () {
+let percentage = index * -100;
+sliderInner.style.transform = "translateX(" + percentage + "%)";
+index++;
+if (index > images.length - 1) { 
+  index = 0;
+}
+}, 5000);
+// FIN SLIDER
